6a20d8ada84a68407e9659e340272c941a997dc4
Grodondo <dragon.carlos@outlook.es>
2024-12-17 20:51:34 +0100

    First attempt, seems like it works, but lacks some touches

--- parser/parser.py
+++ parser/parser.py
@@ -14,9 +14,20 @@ V -> "arrived" | "came" | "chuckled" | "had" | "lit" | "said" | "sat"
 V -> "smiled" | "tell" | "were"
 """
 
+# NP -> N | N N | Det N | Det NP | NP PP | NP Conj NP | Adj NP | NP Adv |
+# DP -> Det NP | Det Adj | Det Adv | Det PP | Det Conj Det
+# VP -> V | V V | V NP | V DP
+# PP -> P NP | P NP Adv
 NONTERMINALS = """
-S -> N V
+S -> NP VP | S Conj S | S Adv | S Adv S | S PP | S PP Adv | S PP Conj S | DP NP
+NP -> N | N N | DP N | NP PP | NP Conj NP | ADP NP | NP Adv | 
+DP -> Det | Det N | Det ADP | Det Adv | Det PP | Det Conj Det
+VP -> V | V V | V NP | V DP
+PP -> P NP | P NP Adv
+ADP -> Adj | ADP Adj | Adj Adv | Adj PP | Adj Conj Adj
+
 """
+# N V Det Adj Adj Adj
 
 grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
 parser = nltk.ChartParser(grammar)
@@ -38,6 +49,7 @@ def main():
 
     # Attempt to parse sentence
     try:
+        print("Parsing sentence...")
         trees = list(parser.parse(s))
     except ValueError as e:
         print(e)
@@ -60,9 +72,22 @@ def preprocess(sentence):
     Convert `sentence` to a list of its words.
     Pre-process sentence by converting all characters to lowercase
     and removing any word that does not contain at least one alphabetic
-    character.
+    character.pytho
     """
-    raise NotImplementedError
+
+    converted_sentence = [word for word in sentence.lower().split()]
+
+    # for word in converted_sentence:
+    # if not any(char.isalpha for char in word):
+    #    converted_sentence.remove(word)
+
+    converted_sentence = [
+        "".join([char for char in word if char.isalpha()])
+        for word in converted_sentence
+    ]
+    print(converted_sentence)
+
+    return converted_sentence
 
 
 def np_chunk(tree):
@@ -72,7 +97,19 @@ def np_chunk(tree):
     whose label is "NP" that does not itself contain any other
     noun phrases as subtrees.
     """
-    raise NotImplementedError
+    np_chunks = []
+
+    for subtree in tree.subtrees(lambda t: t.label() == "NP"):
+        # Check for the last "NP" subtrees on the sentence (It wont show "NP" if it contains another "NP" inside of it)
+        # print(subtree)
+        if not any(
+            child_subtree.label() == "NP"
+            for child_subtree in subtree.subtrees(lambda t: t != subtree)
+        ):
+            np_chunks.append(subtree)
+            print(subtree)
+
+    return np_chunks
 
 
 if __name__ == "__main__":
